20f99959a643bd5b260ef6ab6ef766ec2a3f8754
VimalRaj571 <Vimalraj.ktm95@gmail.com>
2016-12-29 16:41:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,49 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleone = {
+    title : 'Article One | VR',
+    heading : 'Article One',
+    date : 'Dec 29,2016',
+    content : `<p>This first article try to using the app dev tuts This first article try to using the app dev tutsThis first article try to using the app dev tuts</p>
+            <p>This first article try to using the app dev tuts This first article try to using the app dev tutsThis first article try to using the app dev tuts</p>
+            <p>This first article try to using the app dev tuts This first article try to using the app dev tutsThis first article try to using the app dev tuts</p>`
+};
+
+function createTemplate(data){
+    var title = data.title;
+    var heading = data.heading;
+    var date = data.date;
+    var content = data.content;
+    var htmlTemplate = `
+    <!DOCTYPE html> 
+    <html>
+        <head>
+            <title>${title}</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link href="/ui/style.css" rel="stylesheet" />
+        </head>        
+        <body>
+        <div class="stl">
+        <div>  
+            <a href="/" >Home</a>
+        </div>
+        <hr>            
+            ${heading}
+        <div>
+        <div>
+            ${date}
+        </div>
+        <div>
+            ${content}
+        </div>
+        </div>
+        </div>
+        </body>
+    </html>`;
+    return htmlTemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
