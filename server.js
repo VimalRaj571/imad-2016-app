3ac958f1fc6ea138db8d74121af95124a4d52a6b
VimalRaj571 <Vimalraj.ktm95@gmail.com>
2017-01-17 15:44:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -94,7 +94,7 @@ app.get('/counter', function (req, res) {
 //submit-names
 var names= [];
 app.get('/submit-names/:names',function(req,res){
-    var names = req.params.name;
+    var names = req.params.names;
     
     names.push(names);
    //JSON=JS object Notation 
